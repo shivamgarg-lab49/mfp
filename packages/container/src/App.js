d31b64e2b7836419c18e891edae0dd08c7d8212c
Shivam Garg <shivam.garg@lab49.com>
2025-05-09 16:38:35 +0530

    enabled app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-// import MarketingApp from "./components/MarketingApp";
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
   return (
     <div>
       <h1>Hi there!!!</h1>
       <hr />
-      {/* <MarketingApp /> */}
+      <MarketingApp />
     </div>
   );
 };
